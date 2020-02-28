e6cd7109adb81544285705325c051579ecc392a9
chrisfredericks <45493878+chrisfredericks@users.noreply.github.com>
2020-02-27 23:04:47 -0400

    Update LoginTest.php

--- snowDay/tests/Feature/Auth/LoginTest.php
+++ snowDay/tests/Feature/Auth/LoginTest.php
@@ -1,7 +1,5 @@
 <?php
 
-// factory(User::class)->create(); // Creates a user and inserts him into the database
-// factory(User::class)->make(); // Creates a user object
 
 namespace Tests\Feature\Auth;
 
